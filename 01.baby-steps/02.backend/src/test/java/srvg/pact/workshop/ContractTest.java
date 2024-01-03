114e734ec12209c75624feaa7c13565192e0f3b7
Jonatas Fischer <jonatas.fischer@sportradar.com>
2024-01-03 20:51:19 +0100

    updated java project

--- 01.baby-steps/02.backend/src/test/java/srvg/pact/workshop/ContractTest.java
+++ 01.baby-steps/02.backend/src/test/java/srvg/pact/workshop/ContractTest.java
@@ -1,15 +1,14 @@
 package srvg.pact.workshop;
 
-import au.com.dius.pact.core.model.annotations.PactFolder;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @Provider("SportEventStatusProphecyProducer")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 @PactBroker( url = "https://pactbroker.gaming-nonprod.sportradar.online/")
 class ContractTest {
 
