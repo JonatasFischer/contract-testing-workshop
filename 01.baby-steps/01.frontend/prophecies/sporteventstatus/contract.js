d733ff7b109e29fe44dbe353c737667198598717
Jonatas <jonatas.fischer@hotmail.com>
2024-01-05 10:52:28 +0100

    pacts updated

--- 01.baby-steps/01.frontend/prophecies/sporteventstatus/contract.js
+++ 01.baby-steps/01.frontend/prophecies/sporteventstatus/contract.js
@@ -2,11 +2,10 @@ const { PactV3, MatchersV3 } = require("@pact-foundation/pact");
 const prophecy = require('./prophecy');
 const { like } = MatchersV3;
 
-const version = "1.0.8";
+const version = "1.0.11";
 
 const interaction = {
     state: 'initial-prophecy',
-    given: 'an initial  prophecy',
     uponReceiving: 'a request for data',
     withRequest: {
         method: 'GET',
@@ -125,5 +124,125 @@ const interaction = {
         })
     }
 }
+const interactionWithoutState = {
+    given: 'some-given-value',
+    uponReceiving: 'a request for data 2',
+    withRequest: {
+        method: 'GET',
+        path: '/sporteventstatus'
+    },
 
-module.exports = { prophecy, interaction, version };
+    willRespondWith: {
+        status: 200,
+        body: MatchersV3.like({
+            "subject": MatchersV3.equal("second interaction"),
+            "prophecy": [
+                {
+                    "t": MatchersV3.equal(1704304680000),
+                    "v": {
+                        "phase": MatchersV3.equal("live"),
+                        "next_competition_id": 1465009,
+                        "unified_competition_id": "vf:season:1465009",
+                        "next_unified_event_ids": [
+                            "vf:match:135858817"
+                        ],
+                        "next_betstop": 1704304860000,
+                        "unique_tournament_id": 38973,
+                        "competition_id": 1465009,
+                        "event_ids": [
+                            135858816
+                        ],
+                        "next_unified_competition_id": "vf:season:1465009",
+                        "events_start_timestamp": 1704304690000,
+                        "unified_event_ids": [
+                            "vf:match:135858816"
+                        ],
+                        "next_event_ids": [
+                            135858817
+                        ]
+                    }
+                },
+                {
+                    "t": 1704304780000,
+                    "v": {
+                        "phase": MatchersV3.equal("post"),
+                        "next_competition_id": 1465009,
+                        "unified_competition_id": "vf:season:1465009",
+                        "next_unified_event_ids": [
+                            "vf:match:135858817"
+                        ],
+                        "next_betstop": 1704304860000,
+                        "unique_tournament_id": 38973,
+                        "competition_id": 1465009,
+                        "event_ids": [
+                            135858816
+                        ],
+                        "next_unified_competition_id": "vf:season:1465009",
+                        "events_start_timestamp": 1704304690000,
+                        "unified_event_ids": [
+                            "vf:match:135858816"
+                        ],
+                        "next_event_ids": [
+                            135858817
+                        ]
+                    }
+                },
+                {
+                    "t": 1704304800000,
+                    "v": {
+                        "phase": "pre",
+                        "next_competition_id": 1465009,
+                        "unified_competition_id": "vf:season:1465009",
+                        "next_unified_event_ids": [
+                            "vf:match:135858818"
+                        ],
+                        "next_betstop": 1704304860000,
+                        "unique_tournament_id": 38973,
+                        "competition_id": 1465009,
+                        "event_ids": [
+                            135858817
+                        ],
+                        "next_unified_competition_id": "vf:season:1465009",
+                        "events_start_timestamp": 1704304870000,
+                        "unified_event_ids": [
+                            "vf:match:135858817"
+                        ],
+                        "next_event_ids": [
+                            135858818
+                        ]
+                    }
+                },
+                {
+                    "t": 1704304860000,
+                    "v": {
+                        "phase": "live",
+                        "next_competition_id": 1465009,
+                        "unified_competition_id": "vf:season:1465009",
+                        "next_unified_event_ids": [
+                            "vf:match:135858818"
+                        ],
+                        "next_betstop": 1704305040000,
+                        "unique_tournament_id": 38973,
+                        "competition_id": 1465009,
+                        "event_ids": [
+                            135858817
+                        ],
+                        "next_unified_competition_id": "vf:season:1465009",
+                        "events_start_timestamp": 1704304870000,
+                        "unified_event_ids": [
+                            "vf:match:135858817"
+                        ],
+                        "next_event_ids": [
+                            135858818
+                        ]
+                    }
+                }
+            ],
+            "next": 1704304870000,
+            "state": "active",
+            "is_delta": false
+        })
+    }
+}
+
+module.exports = { prophecy, interaction, interactionWithoutState, version };
--- 01.baby-steps/01.frontend/tests/consumer-with-broker.test.js
+++ 01.baby-steps/01.frontend/tests/consumer-with-broker.test.js
@@ -1,7 +1,7 @@
 const { PactV3 } = require('@pact-foundation/pact');
 const axios = require('axios');
 //we import the interaction that we defined at the prophecy with a new name (initialStatusInteraction)
-const { interaction: initialStatusInteraction, prophecy, version: intialStatusVersion } = require('../prophecies/sporteventstatus/contract');
+const { interaction: initialStatusInteraction, prophecy, version: intialStatusVersion, interactionWithoutState } = require('../prophecies/sporteventstatus/contract');
 //from the helper we import the publisher and the configurer that we defined at the helper
 const { publisher: pactPublisher, configurer } = require('../pact.broker.helper');
 
@@ -28,7 +28,26 @@ describe('Define pact with propefy', () => {
                     expect(response.data)
                         .toEqual(prophecy);
                 });
-            }).then(() => pactPublisher(intialStatusVersion));
+            });
+        });
+
+        it('second test', () => {
+            provider.addInteraction(interactionWithoutState);
+            // Define the base URL to the Pact mock server
+            return provider.executeTest(async () => {
+
+                const apiClient = axios.create({ baseURL: pactConfig.baseURL });
+                console.log(`Axios will query on URL ${pactConfig.baseURL}`)
+
+                // Make the request and assert the response
+                return apiClient.get('/sporteventstatus').then(response => {
+                    expect(response.status).toBe(200);
+                    //in a real world you aren't going to assert if you receive the same data that you send, but if your client code is producing the expected result
+                });
+            });
         });
     });
+
+    afterAll(() => pactPublisher(intialStatusVersion));
+    
 });
